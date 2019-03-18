bdd4676b20f3601f2341fcfacc63eb1ee4dbd8a3
Alexandre <alexclemente1985@gmail.com>
2019-03-18 13:34:41 -0300

    Criação de sistema de inserção, edição e exclusão de alunos

--- HelloSpringBoot/src/main/java/br/com/treinaweb/springboot/controllers/AlunosController.java
+++ HelloSpringBoot/src/main/java/br/com/treinaweb/springboot/controllers/AlunosController.java
@@ -1,11 +1,19 @@
 package br.com.treinaweb.springboot.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.treinaweb.springboot.entidades.Aluno;
+import br.com.treinaweb.springboot.entidades.Instituicao;
 import br.com.treinaweb.springboot.repositorios.RepositorioAluno;
+import br.com.treinaweb.springboot.repositorios.RepositorioInstituicao;
 
 
 
@@ -15,9 +23,59 @@ public class AlunosController {
 	@Autowired
 	private RepositorioAluno repositorioAluno;
 	
+	@Autowired
+	private RepositorioInstituicao repositorioInstituicao;
+	
+	@GetMapping("/index")
 	public ModelAndView index() {
 		ModelAndView resultado = new ModelAndView("aluno/index");
+//		List<Aluno> alunos = repositorioAluno.findAll();
 		resultado.addObject("alunos",repositorioAluno.findAll());
+//		resultado.addObject("alunos",alunos);
 		return resultado;
 	}
+	
+	@GetMapping("/inserir")
+	public ModelAndView inserir() {
+		ModelAndView resultado = new ModelAndView("aluno/inserir");
+		Aluno aluno = new Aluno();
+		aluno.setInstituicao(new Instituicao());
+		resultado.addObject("aluno", aluno);
+		
+		resultado.addObject("instituicoes", repositorioInstituicao.findAll());
+		
+		return resultado;
+	}
+	
+	@PostMapping("/inserir")
+	public String inserir(Aluno aluno) {
+		repositorioAluno.save(aluno);
+		return "redirect:/alunos/index"; 
+	}
+	
+	@GetMapping("/editar/{id}")
+	public ModelAndView editar(@PathVariable Long id) {
+		Aluno aluno = repositorioAluno.findOne(id);
+		ModelAndView resultado = new ModelAndView("aluno/editar");
+		resultado.addObject("aluno",aluno);
+		
+		resultado.addObject("instituicoes", repositorioInstituicao.findAll());
+		
+		return resultado;
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Aluno aluno) {
+		repositorioAluno.save(aluno);
+		return "redirect:/alunos/index";
+	}
+	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable Long id) {
+		repositorioAluno.delete(id);
+		return "redirect:/alunos/index";
+	}
+	
 }
+
+
