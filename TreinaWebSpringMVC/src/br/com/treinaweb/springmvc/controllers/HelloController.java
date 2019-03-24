1167021174535fcac24b53f388c29d0fd0ee8af5
alexclemente1985 <alexclemente1985@gmail.com>
2019-03-24 20:24:21 -0300

    Primeiro commit SpringMVC

---
+++ TreinaWebSpringMVC/src/br/com/treinaweb/springmvc/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package br.com.treinaweb.springmvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+	
+	@RequestMapping("/message")
+	public String message() {
+		return "hello";
+	}
+}
